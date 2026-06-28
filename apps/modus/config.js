05c62bfcf8b0ec25a8df2071f22f6e9c8d2271b3
SYSEM <sysem2025@gmail.com>
2026-06-28 12:08:19 +0200

    Aggiungi header anti-cache HTML in .htaccess, ripristina IP diretto in config.js

--- apps/modus/config.js
+++ apps/modus/config.js
@@ -1,6 +1,6 @@
 window.MODUS_CONFIG = {
   apiBase: "http://sysem-modus.duckdns.org:5000",
-  socketHost: "sysem-modus.duckdns.org",
+  socketHost: "79.22.201.131",  // IP diretto invece di hostname
   socketPort: 4059,
   mockMode: false
 };
